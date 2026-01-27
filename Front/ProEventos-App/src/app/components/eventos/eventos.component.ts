2e4c39ee3bcdef6d522ee88a2eb197e16e0ee557
Elisson Bueno <elissonmb@gmail.com>
2026-01-26 21:30:35 -0300

    Seção 8: Reactive Forms e Mudança de Layout

--- Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
@@ -1,12 +1,4 @@
-import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-import { Evento } from '../../models/Evento';
-import { EventoService } from '../../services/evento.service';
-import { Component, OnInit, TemplateRef } from '@angular/core';
-import { Template } from '@angular/compiler/src/render3/r3_ast';
-import { ToastrService } from 'ngx-toastr';
-import { NgxSpinnerService } from 'ngx-spinner';
-
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-eventos',
